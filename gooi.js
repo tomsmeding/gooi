8704a25c2a790cad63d5c3b635ded522543214c2
tomsmeding <tom.smeding@gmail.com>
2016-01-20 10:47:58 +0100

    Initial

---
+++ gooi.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+"use strict";
+
+if(process.argv.length!=3){
+	console.log("Pass the name of the file to throw as a command-line parameter");
+	process.exit();
+}
+
+const http=require("http"),
+      fs=require("fs");
+
+const HTTPHOST="tomsmeding.com";
+const HTTPPORT=11056;
+
+const fname=process.argv[2],
+      safefname=fname.replace(/[\0-\x1f\x7f-\xff\/]/g,"");
+
+let filecontents;
+try {
+	filecontents=fs.readFileSync(fname);
+} catch(e){
+	console.log(`Error while reading file: ${e.message}`);
+	process.exit(1);
+}
+
+let req=http.request({
+	protocol:"http:",
+	hostname:HTTPHOST,
+	port:HTTPPORT,
+	method:"POST",
+	path:`/gooi/${encodeURIComponent(safefname)}`,
+	headers:{
+		"Content-Type":"application/octet-stream",
+		"Content-Length":filecontents.length
+	}
+},(res)=>{
+	if(res.statusCode!=200)console.log(`Couldn't upload: ${res.statusCode}`);
+	res.setEncoding("utf8");
+	res.on("data",(data)=>process.stdout.write(data));
+	res.on("end",()=>process.exit(0));
+});
+req.on("error",(e)=>{
+	console.log(`Request threw an error: ${e.message}`);
+	process.exit(1);
+});
+
+try {
+	req.write(filecontents);
+} catch(e){
+	console.log(`Error while writing file to stream: ${e.message}`);
+	req.close();
+	process.exit(1);
+}
+req.end();
---
+++ gooiserver.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs=require("fs"),
+      http=require("http"),
+      bodyParser=require("body-parser"),
+      app=require("express")(),
+      mkdirp=require("mkdirp"),
+      httpServer=http.Server(app);
+
+const HTTPHOST="tomsmeding.com";
+const HTTPPORT=11056;
+const FILES_DIRNAME="files";
+
+const uniqid=()=>{
+	let i=0;
+	return ()=>i=(i+1)%4294967291; //last prime under 2^32
+}();
+
+mkdirp.sync(FILES_DIRNAME);
+
+let startid;
+try {
+	startid=+fs.readFileSync(`${FILES_DIRNAME}/startid`);
+	if(isNaN(startid)||startId<0)throw 0;
+} catch(_){
+	startid=42424242;
+}
+const genidcode=()=>{
+	let i=startid;
+	return ()=>{
+		const code=`0000000${(i*42%4294967291).toString(36)}`.slice(-7);
+		//(x -> nx) : F_p -> F_p with p prime and 0<n<x is a bijection
+		i++;
+		fs.writeFileSync(`${FILES_DIRNAME}/startid`,i.toString());
+		let res="";
+		for(let j=0;j<7;j++)res+=code[2*j%7]; //same goes here
+		return res;
+	}
+}();
+
+setInterval(()=>{
+	const dirlist=fs.readdirSync(FILES_DIRNAME);
+	const nowtime=new Date().getTime();
+	for(let file of dirlist){
+		const stats=fs.statSync(file);
+		if(!stats.isFile())continue;
+		if(nowtime-stats.mtime.getTime()>3600*1000){ //1 hour storage
+			fs.unlinkSync(file);
+		}
+	}
+},3600*1000); //every hour
+
+app.use(bodyParser.raw({"type":"*/*"}));
+
+app.post("/gooi/:fname",(req,res)=>{
+	const fname=req.params.fname.replace(/[\0-\x1f\x7f-\xff\/]/g,"");
+	if(fname.length==0){
+		res.writeHead(400);
+		res.end("Invalid filename given");
+		return;
+	}
+	const id=genidcode();
+	try {
+		fs.writeFileSync(`${FILES_DIRNAME}/${id}`,req.body);
+		fs.writeFileSync(`${FILES_DIRNAME}/${id}-fname`,fname);
+	} catch(e){
+		console.log(e);
+		res.writeHead(500);
+		res.end();
+		return;
+	}
+	res.writeHead(200);
+	res.end(`http://${HTTPHOST}:${HTTPPORT}/vang/${id}\n`);
+});
+
+app.get("/vang/:id",(req,res)=>{
+	const id=req.params.id.replace(/[^0-9a-z]/g,"").substr(0,10);
+	if(!fs.existsSync(`${FILES_DIRNAME}/${id}`)||!fs.existsSync(`${FILES_DIRNAME}/${id}-fname`)){
+		res.writeHead(404);
+		res.end();
+		return;
+	}
+	const fname=fs.readFileSync(`${FILES_DIRNAME}/${id}-fname`).toString();
+	const fnamequo=`"${fname.replace(/([\\"])/g,"\\$1")}"`
+	res.writeHead(200,{
+		"Content-Type":"application/octet-stream",
+		"Content-Disposition":`attachment; filename=${fnamequo}`
+	});
+	res.end(fs.readFileSync(`${FILES_DIRNAME}/${id}`));
+});
+
+let server=httpServer.listen(HTTPPORT,()=>{
+	console.log(`Server listening on ${server.address().address}:${HTTPPORT}`);
+	console.log(`Configured with host name ${HTTPHOST}`);
+});
