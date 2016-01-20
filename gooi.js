05ab2e6d4b1ef5ed0d71413f97e295555de41d57
tomsmeding <tom.smeding@gmail.com>
2016-01-20 13:51:20 +0100

    option parsing, clipboard saving

--- gooi.js
+++ gooi.js
@@ -1,19 +1,49 @@
 #!/usr/bin/env node
 "use strict";
 
-if(process.argv.length!=3){
-	console.log("Pass the name of the file to throw as a command-line parameter");
-	process.exit();
-}
-
 const http=require("http"),
-      fs=require("fs");
+      fs=require("fs"),
+      toClipboard=require("to-clipboard");
 
 const HTTPHOST="tomsmeding.com",
       HTTPPORT=11056;
 
-const fname=process.argv[2],
-      safefname=fname.replace(/[\0-\x1f\x7f-\xff\/]/g,"?");
+
+function usageandexit(code){
+	console.log("Usage: gooi [-ch] <file>");
+	console.log("Uploads the given file and provides a handy, short-lived, shareable download link.");
+	console.log("  -c  Copy the link to the clipboard");
+	console.log("  -h  Show this");
+	process.exit(code);
+}
+
+let opts={};
+let fname;
+for(let i=2;i<process.argv.length;i++){
+	const arg=process.argv[i];
+	if(arg[0]!='-'){
+		if(!fname)fname=arg;
+		else {
+			console.log("Two files given on the command line; gooi doesn't support multiple-upload yet.");
+			usageandexit(1);
+		}
+		continue;
+	}
+	for(let j=1;j<arg.length;j++){
+		if("ch".indexOf(arg[j])==-1){
+			console.log(`Unrecognised flag '${arg[j]}'`);
+			usageandexit(1);
+		}
+		opts[arg[j]]=true;
+	}
+}
+if(opts.h)usageandexit(0);
+if(!fname){
+	console.log("No filename given!");
+	usageandexit(1);
+}
+
+const safefname=fname.replace(/[\0-\x1f\x7f-\xff\/]/g,"?");
 
 let filecontents;
 try {
@@ -34,10 +64,20 @@ let req=http.request({
 		"Content-Length":filecontents.length
 	}
 },(res)=>{
-	if(res.statusCode!=200)console.log(`Couldn't upload: ${res.statusCode}`);
+	const success=res.statusCode==200;
+	if(!success)console.log(`Couldn't upload: ${res.statusCode}`);
 	res.setEncoding("utf8");
-	res.on("data",(data)=>process.stdout.write(data));
-	res.on("end",()=>process.exit(0));
+	let bodytext="";
+	res.on("data",(data)=>{
+		if(success)bodytext+=data;
+		process.stdout.write(data)
+	});
+	res.on("end",()=>{
+		if(success){
+			toClipboard.sync(bodytext.trim());
+			console.log("(copied)");
+		}
+	});
 });
 req.on("error",(e)=>{
 	console.log(`Request threw an error: ${e.message}`);
