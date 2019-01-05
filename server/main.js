82d398fa93b5ea9f0bbf0e253024b786792e2ea7
tomsmeding <tom.smeding@gmail.com>
2019-01-05 15:38:15 +0100

    Detect valid utf8 as text/plain

--- server/main.js
+++ server/main.js
@@ -65,6 +65,24 @@ if (HOURS_RETENTION > 0) {
 	},3600*1000); //every hour
 }
 
+// Returns true if the buffer can be the initial part of a valid utf-8 encoded string
+function validUTF8Head(buf) {
+	let state = 0;  // 0 = ascii, >0 = that number of 0b10... bytes remaining
+	for (let i = 0; i < buf.length; i++) {
+		if (state == 0) {
+			if ((buf[i] & 0x80) == 0) continue;
+			if ((buf[i] & 0xf8) == 0xf0) state = 3;
+			else if ((buf[i] & 0xf0) == 0xe0) state = 2;
+			else if ((buf[i] & 0xe0) == 0xc0) state = 1;
+			else if ((buf[i] & 0x80) != 0) return false;
+		} else {
+			if ((buf[i] & 0xc0) == 0x80) state--;
+			else return false;
+		}
+	}
+	return true;
+}
+
 function getMime(filedesc) {
 	const buffer = Buffer.alloc(fileType.minimumBytes);
 
@@ -76,7 +94,9 @@ function getMime(filedesc) {
 
 	const res = fileType(buffer);
 
-	return res && res.mime;
+	if (res) return res.mime;
+	if (validUTF8Head(buffer)) return "text/plain";
+	return null;
 };
 
 const idMiddleware = function (req, res, next) {
